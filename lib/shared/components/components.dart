237f767bba99f85814f1b0bc492d851d0512cd5e
Ahmed Abdelhamid <121736377+Ahmed-Abdelhamid0@users.noreply.github.com>
2023-02-28 20:41:12 +0200

    Update components.dart

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -2,35 +2,6 @@ import 'package:conditional_builder/conditional_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:untitled1/shared/cubit/cubit.dart';
 
-Widget defaultButton({
-   double width=double.infinity,
-   Color background=Colors.red,
-  bool isUpperCase=true,
-  double radius=15.0,
-  required Function function,
-  required String text,
-})=>
-    Container(
-  width: width,
-  height: 40.0,
-  child: MaterialButton(
-    onPressed: ()
-    {
-      function();
-    },
-    child: Text(
-      isUpperCase ? text.toUpperCase(): text,
-      style: TextStyle(
-        color: Colors.white,
-
-      ),
-    ),
-  ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(radius),
-        color: background,
-      ),
-);
 
 
 //TO-DO
